e4bedfdd8a46ac97fa5b94de7c1c0ed51fd8baf8
Vladimir Tryasunov <tryasunov933@gmail.com>
2024-11-04 09:27:16 +0200

    fix connection

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import { fetchImages } from '/js/pixabay-api';
-import { renderGallery } from '/js/render-functions';
+import { fetchImages } from './js/pixabay-api';
+import { renderGallery } from './js/render-functions.js';
 
 const form = document.getElementById('search-form');
 const gallery = document.getElementById('gallery');
