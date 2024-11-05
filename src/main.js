e59ceec0fa505e63a98c88891104d7af4b17b7a7
Vladimir Tryasunov <tryasunov933@gmail.com>
2024-11-05 13:05:20 +0200

    fix main js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { fetchImages } from './js/pixabay-api';
 import { renderGallery } from './js/render-functions.js';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 const form = document.getElementById('search-form');
 const gallery = document.getElementById('gallery');
@@ -11,8 +13,13 @@ form.addEventListener('submit', async event => {
   const query = form.elements.query.value.trim();
   if (query === '') {
     iziToast.warning({
-      title: 'Warning',
+      position: 'topRight',
       message: 'Please enter a search query.',
+      messageSize: '16',
+      backgroundColor: '#ef4040',
+      messageColor: '#fafafb',
+      maxWidth: '432px',
+      messageLineHeight: '88px',
     });
     return;
   }
@@ -25,6 +32,10 @@ form.addEventListener('submit', async event => {
     renderGallery(data.hits);
   } catch (error) {
     console.error('Error fetching images:', error);
+    iziToast.error({
+      title: 'Error',
+      message: 'Failed to load images. Try again.',
+    });
   } finally {
     loader.classList.add('hidden');
   }
